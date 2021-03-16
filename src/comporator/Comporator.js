5ce37dc330920aaf7cdcdcd9bc16ad445a7980f8
Viktor Bykov <viktor.bykov.kr@gmail.com>
2021-03-16 17:36:30 +0200

    Code optimization

--- src/comporator/Comporator.js
+++ src/comporator/Comporator.js
@@ -10,96 +10,93 @@ class Comporator extends React.Component {
             currentPokemon: 0,
             pokemonData: [],
             pokemonParsedImages: [],
-            pokemonCurrentImage: 0,
+            pokemonImageIndex: 0,
             isLoaded: false,
-            error: false,
+            isError: false,
+            errorMessage: 'Pokemon not found, check your input',
+            pokemonInputText: 'Введіть ID або ім\'я покемона:',
         };
     }
 
 
     // Get pokemon stat
     getPokemonStats() {
-        if (this.state.isLoaded) {
-            return this.state.pokemonData.stats.map((statItem, index) => (
-                <li key={index}>
-                    <span className="stat_name">{statItem.stat.name}</span>
-                    <span className="stat_data">{statItem.base_stat}</span>
-                </li>
-            ));
-        }
+        return this.state.pokemonData.stats.map((statItem, index) => (
+            <li key={index}>
+                <span className="stat_name">{statItem.stat.name}</span>
+                <span className="stat_data">{statItem.base_stat}</span>
+            </li>
+        ));
     }
 
 
     // Get active pokemon image
     getPokemonImg() {
-        if (this.state.isLoaded) {
-            return <img src={this.state.pokemonParsedImages[this.state.pokemonCurrentImage]} alt="" />
-        }
+        return <img src={this.state.pokemonParsedImages[this.state.pokemonImageIndex]} alt="" />
     }
 
 
-    // Get new pokemon foto by pressing next/prev buttons
-    switchFoto = (event) => {
-        const direction = event.target.value;
+    // Get new pokemon image by pressing next/prev buttons
+    switchFoto = (e) => {
+        const { pokemonImageIndex } = this.state;
+
+        const direction = e.target.value;
         switch (direction) {
+            // previous image
             case 'prev':
-                if (this.state.pokemonCurrentImage !== 0) {
+                if (pokemonImageIndex !== 0) {
                     this.setState({
-                        pokemonCurrentImage: this.state.pokemonCurrentImage - 1,
+                        pokemonImageIndex: this.state.pokemonImageIndex - 1,
                     });
                 }
                 break;
+            // next image
             case 'next':
-                if (this.state.pokemonCurrentImage !== this.state.pokemonParsedImages.length - 1) {
+                if (pokemonImageIndex !== this.state.pokemonParsedImages.length - 1) {
                     this.setState({
-                        pokemonCurrentImage: this.state.pokemonCurrentImage + 1,
+                        pokemonImageIndex: this.state.pokemonImageIndex + 1,
                     });
                 }
                 break;
-        }
-    }
-
-
-    // Display error message
-    errorMessage() {
-        if (this.state.error){
-           return <span className="error_message">Pokemon not found, check your input</span>;
         }
     }
 
 
     // Pars all images of current pokemon
     parsImages() {
-        if (this.state.isLoaded) {
-            const result = [];
-            // recursive images parsing
-            function parsInnerImages(obj) {
-                for (const prop in obj) {
-                    const value = obj[prop];
-                    if (value !== undefined && value !== null) {
-                        if (typeof value === 'object') {
-                            parsInnerImages(value);
-                        } else {
-                            result.push(value);
-                        }
+        const result = [];
+        // recursive images parsing
+        function parsInnerImages(obj) {
+            for (const prop in obj) {
+                const value = obj[prop];
+                if (value !== undefined && value !== null) {
+                    if (typeof value === 'object') {
+                        parsInnerImages(value);
+                    } else {
+                        result.push(value);
                     }
                 }
-                return result;
             }
-
-            parsInnerImages(this.state.pokemonData.sprites);
-
-            this.setState({
-                pokemonParsedImages: result,
-            });
-
+            return result;
         }
+
+        parsInnerImages(this.state.pokemonData.sprites);
+
+        this.setState({
+            pokemonParsedImages: result,
+        });
     }
 
 
     // Get random pokemon on load
     getRandomPokemonId() {
         return Math.floor(Math.random() * 899);
+    }
+
+
+    // Show error message
+    errorMessage() {
+        return <span className="error_message">{this.state.errorMessage}</span>;
     }
 
 
@@ -121,13 +118,13 @@ class Comporator extends React.Component {
             .then((data) => {
                 if (data === undefined) {
                     this.setState({
-                        error: true,
+                        isError: true,
                     });
                 } else {
                     this.setState({
                         pokemonData: data,
                         isLoaded: true,
-                        error: false,
+                        isError: false,
                     });
                     this.parsImages();
                 }
@@ -143,6 +140,9 @@ class Comporator extends React.Component {
     render() {
         const { subTitle } = this.state;
         const { pokemonData } = this.state;
+        const { isLoaded } = this.state;
+        const { isError } = this.state;
+        const { pokemonInputText } = this.state;
 
         return (
             <div>
@@ -150,16 +150,16 @@ class Comporator extends React.Component {
                 <div className="comporator_content">
                     <div className="pokemon_cards__wrapper">
                         <div className="pokemon_cards__input_wrapper">
-                            <p>Введіть ID або ім'я покемона:</p>
-                            <input onChange={this.getPokemonData} name="id_name" />
-                            {this.errorMessage()}
+                            <p>{pokemonInputText}</p>
+                            <input onChange={this.getPokemonData} name="id_name" className={isError ? 'error' : null}/>
+                            {isError && this.errorMessage()}
                         </div>
                         <div className="pokemon_card">
                             <span className="pokemon_name">{pokemonData.name}</span>
                             <div className="pokemon_card__info_wrapper">
                                 <div className="pokemon_card__left_wrapper">
                                     <div className="pokemon_card__foto">
-                                        {this.getPokemonImg()}
+                                        {isLoaded && this.getPokemonImg()}
                                     </div>
                                     <div className="pokemon_card__foto_buttons">
                                         <button value='prev' onClick={this.switchFoto}>Prev</button>
@@ -167,7 +167,7 @@ class Comporator extends React.Component {
                                     </div>
                                 </div>
                                 <div className="pokemon_card__stats">
-                                    {this.getPokemonStats()}
+                                    {isLoaded && this.getPokemonStats()}
                                 </div>
                             </div>
                         </div>
